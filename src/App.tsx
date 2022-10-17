9654f5a03e07ece1c318352ae398d4a5bc1cf832
Nadav Kirsh <saturdaysyouth@gmail.com>
2022-10-17 17:41:29 +0200

    fetch data from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
 import React from "react";
-
+import { useEffect, useState } from "react";
 import "./App.scss";
 
+type EventT = {
+    id: string;
+    title: string;
+    flyerFront: string;
+    location: number;
+    startTime: string;
+    endTime: string;
+    date: string;
+};
+
 function App() {
+    const [events, setEvents] = useState<EventT[]>();
+
+    useEffect(() => {
+        fetch("https://tlv-events-app.herokuapp.com/events/uk/london")
+            .then((response) => response.json())
+            .then((data) => setEvents(data));
+    }, []);
+
+    console.log(events);
+
     return (
         <div className="App">
             <header className="App-header"></header>
