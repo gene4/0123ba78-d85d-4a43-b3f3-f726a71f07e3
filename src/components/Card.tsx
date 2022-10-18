4d9827923b0092e4722e0d7da0c0915b70493603
Nadav Kirsh <saturdaysyouth@gmail.com>
2022-10-18 12:28:23 +0200

    Card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,56 @@
+import "./Card.scss";
+import stockEventImage from "../assets/stock-event-image.jpeg";
+import moment from "moment";
+
+interface Props {
+    title: string;
+    flyerFront?: string;
+    venue: {
+        name: string;
+        direction: string;
+    };
+    startTime: string;
+    endTime: string;
+    addEventToCart: () => void;
+}
+
+function Card({
+    title,
+    flyerFront,
+    venue,
+    startTime,
+    endTime,
+    addEventToCart,
+}: Props) {
+    return (
+        <div className="card">
+            {
+                <img
+                    alt={title}
+                    src={flyerFront ? flyerFront : stockEventImage}
+                />
+            }
+            <a rel="noreferrer" target={"_blank"} href={venue.direction}>
+                <img
+                    alt="open-location"
+                    className="location-btn"
+                    src="/location.svg"
+                />
+                {venue.name}
+            </a>
+            <h2>{title}</h2>
+            <p>Starts: {moment(startTime).format("DD.MM.YYYY, H:mm")} </p>
+            <p>Ends: {moment(endTime).format("DD.MM.YYYY, H:mm")} </p>
+
+            <div className="action-container">
+                <button onClick={addEventToCart}>
+                    {" "}
+                    <img alt="add-event" className="add-btn" src="/add.svg" />
+                    Add event
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
