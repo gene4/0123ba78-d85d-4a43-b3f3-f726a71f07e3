2b358ddc9dcaf093e88e8596349864ebad785f92
Nadav Kirsh <saturdaysyouth@gmail.com>
2022-10-22 20:42:07 +0200

    sponner component

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Spinner.scss";
+
+function Spinner() {
+    return (
+        <div className="spinner-container">
+            <div className="lds-ring">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    );
+}
+
+export default Spinner;
